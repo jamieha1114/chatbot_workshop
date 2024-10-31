20b13c48c18c209b00e5559af822f694d400fb8d
jamieha1114 <jamieha1114@snu.ac.kr>
2024-10-31 10:40:01 +0000

    첫 웹사이트!

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,10 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.title("👋🏻 Streamlit 앱 만들기")
+st.subheader("예비교원 워크숍 10월 특강 실습 페이지")
+st.success("streamlit으로 만든 페이지에 오신 것을 환영합니다!")
+st.info("streamlit으로 만든 페이지에 오신 것을 환영합니다!")
+st.warning("streamlit으로 만든 페이지에 오신 것을 환영합니다!")
+st.image("https://media.giphy.com/media/UkfUod3TFW66J2BWKK/giphy.gif?cid=790b7611i80c6cvqr4szgw34juet4k0syah56llhyfghx6m7&ep=v1_gifs_trending&rid=giphy.gif&ct=g", caption="Welcome to ZZOLAB") 
+st.link_button("연결할 url을 이 다음 변수에 써주세요!", 'https://docs.streamlit.io/develop/quick-reference/cheat-sheet')
